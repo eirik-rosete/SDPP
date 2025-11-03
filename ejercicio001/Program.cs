3800a6e4f5506979ab3d6d300370cefa8f10db0c
eirik-rosete <eirik.rosete@gmail.com>
2025-11-03 04:04:56 +0100

    feat: adding first glimpse into c#

---
+++ ejercicio001/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ejercicio001
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            calculadora.Imprimir();
+        }
+    }
+}
