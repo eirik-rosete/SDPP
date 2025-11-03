d5acd1566fb54d359bd784d22fa0af78a2e82b85
eirik-rosete <eirik.rosete@gmail.com>
2025-11-03 07:36:46 +0100

    feat: implementing first advancement for prime numbers problem solution

---
+++ ejercicio004/Varios.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio004
+{
+    public class Varios
+    {
+        private int rangoDeNumeros = 30;
+
+        public static int[] MostrarPrimos(int rangoDeNumeros)
+        {
+            int contador = 0;
+
+            for (int i = 2; i <= rangoDeNumeros; i++) 
+            {
+                if (EsPrimo(i))
+                {
+                    contador++;
+                }
+                    
+            }
+
+            int[] primos = new int[contador];
+
+            int indice = 0;
+            for (int i = 2; i <= rangoDeNumeros; i++) 
+            {
+                if (EsPrimo(i))
+                {
+                    primos[indice] = i;
+                    indice++;
+                }
+            }
+
+            return primos;
+        }
+        public static int[] MostrarNoPrimos(int rangoDeNumeros, int[] primos)
+        {
+            int[] noPrimos = new int[rangoDeNumeros - primos.Length];
+
+            int indice = 0;
+            int indicePrimos = 0;
+            for (int i = 1; i <= rangoDeNumeros; i++)
+            {
+                if (indicePrimos >= primos.Length || i != primos[indicePrimos])
+                {
+                    noPrimos[indice] = i;
+                    indice++;
+                }
+                else 
+                {
+                    indicePrimos++;
+                }
+            }
+
+            return noPrimos;
+        }
+        private static bool EsPrimo(int numero)
+        {
+            if (numero <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
